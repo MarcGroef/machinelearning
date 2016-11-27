d63d3fd5c631eec2c56e2a0807ba4427fe369b9c
= <=>
2016-11-27 17:04:54 +0100

    added mlp

---
+++ code/includes/MLP.py
@@ -0,0 +1,52 @@
+import numpy as np
+
+
+class MLP():
+  
+   nLayers = 2
+   
+   
+   
+   def __init__(self, nInputNodes, hiddenSize, outputSize):
+     self.hiddenWeights = np.random.rand(nInputNodes, hiddenSize)
+     self.outputWeights = np.random.rand(hiddenSize, outputSize)
+     self.hiddenBias = np.zeros(hiddenSize)
+     self.outputBias = np.zeros(outputSize)
+     pass
+   
+   def sigmoid(self,x):
+     return 1.0 / (1.0 + np.exp(-1. * x))
+   
+   def d_sigmoid(self,x):
+     sig = self.sigmoid(x)
+     return sig * (1. - sig)
+   
+   def process(self, inputArray):
+     self.hiddenNodes = self.sigmoid(inputArray.dot(self.hiddenWeights) + self.hiddenBias)
+     self.outputNodes = self.sigmoid(self.hiddenNodes.dot(self.outputWeights) + self.outputBias)
+     return self.outputNodes
+   
+   def train(self, inputArray, targetOut, learningRate):
+     error = targetOut - self.process(inputArray)
+     loss = 0.5 * error * error
+     
+
+     delta = (error) * self.d_sigmoid(self.outputNodes) 
+     updateBiasOut = delta
+     
+     updateWeightsOut = np.transpose(np.outer(delta , self.hiddenNodes))
+     delta = delta.dot(np.transpose(self.outputWeights)) * (self.d_sigmoid(self.hiddenNodes))
+     
+     updateBiasHidden = delta
+     updateWeightsHidden = np.transpose(np.outer(delta, inputArray))
+
+     self.outputWeights += learningRate * updateWeightsOut
+     self.outputBias += learningRate * updateBiasOut
+     self.hiddenBias += learningRate * updateBiasHidden
+     self.hiddenWeights += learningRate * updateWeightsHidden
+     return loss
+     
+     
+     
+     
+     
---
+++ code/main.py
@@ -0,0 +1,26 @@
+from includes.MLP import MLP
+import numpy as np
+
+
+if __name__ == "__main__":
+  
+  xorIn1 = np.array([0,1])
+  xorIn2 = np.array([1,0])
+  xorIn3 = np.array([1,1])
+  xorIn4 = np.array([0,0])
+  
+  xorOut1 = np.array([1])
+  xorOut2 = np.array([1])
+  xorOut3 = np.array([0])
+  xorOut4 = np.array([0])
+  
+  
+  nn = MLP(2, 30, 1)
+  for iter in range(200000):
+    loss = 0
+    loss += nn.train(xorIn1, xorOut1, 0.051)
+    loss += nn.train(xorIn2, xorOut2, 0.051)
+    loss += nn.train(xorIn3, xorOut3, 0.051)
+    loss += nn.train(xorIn4, xorOut4, 0.051)
+    print loss
+  
